a77f1c8dd0e7bfe5cd879d416560495c6604763d
Manikandan <manikandanmsm999@gmail.com>
2022-12-20 08:31:14 +0530

    added home component, page not found component and navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import {HomeComponent} from './home/home.component';
+import {CompaniesManagementComponent} from './companies-management/companies-management.component';
+import {UsersManagementComponent} from './users-management/users-management.component';
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
+
+const routes: Routes = [
+  {path:'home',component:HomeComponent},
+  {path:'',redirectTo:'/home',pathMatch:'full'},
+  {path:'companies',component:CompaniesManagementComponent},
+  {path:'users',component:UsersManagementComponent},
+  {path:'**',component:PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CompaniesManagementComponent } from './companies-management/companies-management.component';
 import { UsersManagementComponent } from './users-management/users-management.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CompaniesManagementComponent,
-    UsersManagementComponent
+    UsersManagementComponent,
+    HomeComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
